1c7d5ea359830ae51e799162402b356b7873c1a2
Hengfeng Li <hengf.li@gmail.com>
2018-12-05 21:33:59 +1100

    Update the setup.py.

--- setup.py
+++ setup.py
@@ -11,6 +11,25 @@ from pathlib import Path
 
 README = Path('README.md')
 
+# -*- Classifiers -*-
+
+classes = """
+    Development Status :: 3 - Alpha
+    License :: OSI Approved :: MIT License
+    Programming Language :: Python :: 3 :: Only
+    Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
+    Programming Language :: Python :: Implementation :: CPython
+    Operating System :: POSIX
+    Operating System :: Microsoft :: Windows
+    Operating System :: MacOS :: MacOS X
+    Operating System :: Unix
+    Environment :: No Input/Output (Daemon)
+    Intended Audience :: Developers
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
+
 # -*- Long Description -*-
 
 
@@ -31,6 +50,7 @@ setup(
     package_dir={'kcpy': 'kcpy'},
     include_package_data=True,
     python_requires='>=3.5.0',
+    classifiers=classifiers,
     keywords=[
         'kinesis',
         'consumer',
